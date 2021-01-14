afa9b3b6b7540b2932623fdb302cab0247e6515a
Dror Tirosh <dror@opengsn.org>
2021-01-14 21:00:07 +0200

    Added "How it works" explanation page.

---
+++ workspaces/react-app/src/components/CtfInfo.jsx
@@ -0,0 +1,16 @@
+import React from 'react'
+
+export const CtfInfo = () => <><p/><table border="1" style={{fontSize:-5, textAlign: "left"}}>
+	<h2>How does it work ?</h2>
+    In simple demonstration, you can click the button to "capture the flag".<br/>
+    All transactions go through GSN - you are only asked to sign, and not to pay for the transactions<br/>
+    You can click the CTF contract link, and see that it indeed has its state changed and emitted events - but only through internal transactions.
+    The actual transaction is always on the RelayHub<p/>
+    The paymaster that pays for the transaction is also not called directly, but as internal transaction from the same RelayHub transcation.
+    <p/>
+    NOTE: if the paymaster's balance is too low, you can "top it up". <p/>
+    For the purpose of this demo, the paymaster is very naive paymaster, and will accept to pay for any request.
+</table>    
+</>
+
+
