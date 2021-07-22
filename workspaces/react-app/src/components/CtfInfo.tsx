a4479822a1eb9840924d313e32a6d20c0e396ca3
Dror Tirosh <dror@opengsn.org>
2021-07-22 13:30:09 +0300

    typescript

--- workspaces/react-app/src/components/CtfInfo.tsx
+++ workspaces/react-app/src/components/CtfInfo.tsx
@@ -1,12 +1,14 @@
+// @ts-ignore
 import React from 'react'
 
+// @ts-ignore
 export const CtfInfo = () => <><p/><table border="1" style={{fontSize:-5, textAlign: "left"}}><tbody><tr><td>
 	<h2>How does it work ?</h2>
     In simple demonstration, you can click the button to "capture the flag".<br/>
     All transactions go through GSN - you are only asked to sign, and not to pay for the transactions<br/>
     You can click the CTF contract link, and see that it indeed has its state changed and emitted events - but only through internal transactions.
     The actual transaction is always on the RelayHub<p/>
-    The paymaster that pays for the transaction is also not called directly, but as internal transaction from the same RelayHub transcation.
+    The paymaster that pays for the transaction is also not called directly, but as internal transaction from the same RelayHub transaction.
     <p/>
     NOTE: if the paymaster's balance is too low, you can send some test eth to it, to "top it up". <p/>
     For the purpose of this demo, the paymaster is very naive paymaster, and will accept to pay for any request.
---
+++ workspaces/react-app/src/components/GsnStatus.tsx
@@ -0,0 +1,92 @@
+// @ts-ignore
+import React, {Component} from 'react';
+import {Address} from "./utils";
+import {Ctf, GsnStatusInfo} from "@ctf/eth/src/Ctf";
+import {ethers} from "ethers";
+
+declare let global: { network: any }
+
+interface GsnStatusState {
+  relayHubAddress?: string
+  totalRelayers?: string
+  paymasterAddress?: string
+  paymasterBalance?: string
+  forwarderAddress?: string
+}
+
+interface GsnStatusProps {
+  ctf?: Ctf
+}
+
+export class GsnStatus extends Component<GsnStatusProps, GsnStatusState> {
+  ctf: Ctf
+
+  state: GsnStatusState = {}
+
+  constructor(props: GsnStatusProps) {
+    super(props)
+    this.ctf = props.ctf!
+  }
+
+
+  async initProvider(): Promise<GsnStatusInfo> {
+    // @ts-ignore
+    if (!this.ctf.gsnProvider.relayClient.initialized) {
+      console.log('not initialized. calling provider.init')
+      await this.ctf.gsnProvider.init()
+      console.log('after provider.init')
+    }
+    return await this.ctf.getGsnStatus()
+  }
+
+  init() {
+    const setState = this.setState.bind(this)
+    console.log('== init before')
+    this.initProvider().then(gsnStatus => {
+      console.log('== after getGsnStatus', gsnStatus)
+      setState({
+        relayHubAddress: gsnStatus.relayHubAddress,
+        forwarderAddress: gsnStatus.forwarderAddress,
+        paymasterAddress: gsnStatus.paymasterAddress,
+      });
+      gsnStatus.getPaymasterBalance().then(bal => {
+        const b= parseFloat(bal.toString())
+        console.log('bal=', b)
+        setState({
+          paymasterBalance: (b/1e18).toFixed(6)
+        })
+      })
+      gsnStatus.getActiveRelayers().then(count => {
+        setState({
+          totalRelayers: count.toString()
+        })
+      })
+    })
+  }
+
+  componentDidMount() {
+    this.init()
+  }
+
+  render() {
+    const {relayHubAddress, totalRelayers, paymasterAddress, paymasterBalance, forwarderAddress} = this.state
+
+    return (
+      // @ts-ignore
+      <table border={1}
+             style={{textAlign: "left"}}>
+        <tbody>
+        <tr>
+          <td>
+            <b>GSN Contracts Status:</b><br/>
+            Current network: {global.network.name}<br/>
+            RelayHub <Address addr={relayHubAddress}/> {totalRelayers && <>relayers: {totalRelayers}</>}<br/>
+            Paymaster <Address addr={paymasterAddress}/> balance: {paymasterBalance} <br/>
+            Forwarder <Address addr={forwarderAddress}/><br/>
+          </td>
+        </tr>
+        </tbody>
+      </table>
+    );
+  }
+}
