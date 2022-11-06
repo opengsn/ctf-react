12896568bff67906fd933cb7eca2053d4b354c9d
qq7 <qq7dev@gmail.com>
2022-11-06 13:25:32 +0100

    add rel='noopener noreferrer' to links

--- workspaces/react-app/src/App.tsx
+++ workspaces/react-app/src/App.tsx
@@ -7,7 +7,7 @@ import {CtfInfo} from './components/CtfInfo'
 function App() {
 
   const NoWalletInstalledWarning = () => <div>
-     This example page requires a browser wallet (e. g. <a href="https://metamask.io/download/" target="_blank">MetaMask</a> or <a href="https://brave.com/wallet/" target="_blank">Brave</a>) to work.
+     This example page requires a browser wallet (e. g. <a href="https://metamask.io/download/" target="_blank" rel="noopener noreferrer">MetaMask</a> or <a href="https://brave.com/wallet/" target="_blank" rel="noopener noreferrer">Brave</a>) to work.
     </div>
   
 
