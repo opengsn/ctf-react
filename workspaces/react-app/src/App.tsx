a5acb14836bc001daa1279570ad47436f85889a9
qq7 <qq7dev@gmail.com>
2022-11-25 22:07:39 +0100

    No browser wallet installed warning (#13)
    * catch 'window.ethereum is undefined' in react app
    
    * add rel='noopener noreferrer' to links

--- workspaces/react-app/src/App.tsx
+++ workspaces/react-app/src/App.tsx
@@ -5,10 +5,18 @@ import {CaptureTheFlag} from './components/CaptureTheFlag'
 import {CtfInfo} from './components/CtfInfo'
 
 function App() {
+
+  const NoWalletInstalledWarning = () => <div>
+     This example page requires a browser wallet (e. g. <a href="https://metamask.io/download/" target="_blank" rel="noopener noreferrer">MetaMask</a> or <a href="https://brave.com/wallet/" target="_blank" rel="noopener noreferrer">Brave</a>) to work.
+    </div>
+  
+
   return (
     <div className="App">
       <header className="App-header">
-        <CaptureTheFlag/>
+        { window.ethereum !== undefined
+        ? <CaptureTheFlag/>
+        : <NoWalletInstalledWarning /> }
 	<CtfInfo/>
       </header>
     </div>
